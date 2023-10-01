6ed2cbbccf9a6a77bda7f7374aed13ec1596f7c8
Deepanshukesarwani <deepanshukesarwani7@gmail.com>
2023-10-01 17:31:23 +0530

    setUp commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>to kaise hai aap log</h1>
+    </div>
+  )
+}
+
+export default App
